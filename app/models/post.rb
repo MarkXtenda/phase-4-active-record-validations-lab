bb5f8721a4ccb6bfe53b0df4dce9a5c7ead9a2c8
MarkXtenda <mark.i2p.1998@gmail.com>
2022-11-28 21:43:53 +1100

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :checky
+
+    private
+
+    def checky
+        if title.present? && title.include?('True Facts')
+            if title != "You Won't Believe These True Facts"
+                errors.add(:title, "can't include phrase: 'True Facts'")
+            end
+        end
+    end
+
 end
